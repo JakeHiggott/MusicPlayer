d94586be1478ffe7b5b5a3e39fce817d77a7f5e4
Jake Higgot <jakehiggott1@gmail.com>
2018-06-18 11:06:17 -0400

    Initial Commit

---
+++ MusicPlayerApp/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  MusicPlayerApp
+//
+//  Created by Jake Higgot on 6/18/18.
+//  Copyright © 2018 Jake Higgott. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
